345c86f1074002c948f299f309a796ff6c591a35
Dukewilz <safwanzw20@gmail.com>
2026-08-28 19:56:51 +0700

    Menggabungkan proyek lokal ke repositori existing

---
+++ hypr/hyprland/colors.lua
@@ -0,0 +1,16 @@
+hl.config({
+    general = {
+        col = {
+            active_border   = "rgba(53443e77)",
+            inactive_border = "rgba(231a1633)",
+        },
+    },
+    misc = {
+        background_color = "rgba(1a110eFF)",
+    },
+})
+
+hl.window_rule({
+    match        = { pin = 1 },
+    border_color = "rgba(ffb597AA) rgba(ffb59777)",
+})
